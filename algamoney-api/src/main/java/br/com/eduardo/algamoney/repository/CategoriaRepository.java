70b3943cc69dda77d6cb42438213d473a94f3a04
Eduardo Alves dos Santos <santosedu@live.com>
2019-08-05 11:00:26 -0300

    Novo commit

--- algamoney-api/src/main/java/br/com/eduardo/algamoney/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.eduardo.algamoney.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.eduardo.algamoney.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Long>{
-
-}
